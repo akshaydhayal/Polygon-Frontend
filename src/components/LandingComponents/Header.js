041dcde9c1c04430cf9d39701d8c8af418c6d782
Akshay Dhayal <akshaydhayalkd99@gmail.com>
2021-12-07 15:29:04 +0530

    Added Metamask wallet connection

--- src/components/LandingComponents/Header.js
+++ src/components/LandingComponents/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useEffect, useState } from 'react';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
@@ -9,6 +10,57 @@ import FormControl from "react-bootstrap/FormControl";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+    const connectWalletButton = () => {
+    return (
+      <Button variant="primary" active onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet</Button>
+    )
+  }
+
+    useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
     return (
         <Navbar bg="dark" variant="dark">
           <img className="navv-logo1" src="https://image.shutterstock.com/image-vector/minimalist-simple-modern-ocean-sun-260nw-1892993923.jpg"
@@ -29,14 +81,17 @@ function Header() {
             </NavDropdown>
           </Nav>
           
-          <Button variant="dark" active>Users Login</Button>{' '}
+          <Button variant="light" style={{margin: '10px'}} active>Users Login</Button>{' '}
 
-          <Nav>
+         {/*  <Nav>
             <Nav.Link href="#deets"><Button variant="light" active>Button 1</Button></Nav.Link>
-            <Nav.Link href="#deets"><Button variant="light" active>Button 2</Button></Nav.Link>
-       </Nav>
+            <Nav.Link href="#deets"><Button variant="light" active>{connectWalletButton()}</Button></Nav.Link>
+       </Nav> */}
 
         </Navbar.Collapse>
+
+       <div>{currentAccount ? <Button style={{color: "white"}}>Wallet Connected!</Button> : connectWalletButton()}</div>
+
       </Navbar>
     )
 }
